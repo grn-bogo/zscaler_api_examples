e686b3865d70965f389f822b0d2bca850ddc03dc
grn-bogo <bogumilgriner@gmail.com>
2021-05-24 23:41:58 +0200

    added department selection via zs export file

--- zs_api.py
+++ zs_api.py
@@ -291,7 +291,7 @@ class APIManager:
     def parse_departments_to_process(self, departments_csv_file):
         with open(departments_csv_file, 'r') as deps_f:
             reader = csv.reader(deps_f)
-            data = [row for row in reader][0]
+            data = [row[0] for row in reader]
             self._selected_departments = [dep for dep in data if dep in self._departments_dict]
         print(F'WILL PROCESS DEPARTMENTS: {self._selected_departments}')
 
